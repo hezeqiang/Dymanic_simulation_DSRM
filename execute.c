ea61c25179da766d98361383401a62513717e685
he <56215142+hezeqiang@users.noreply.github.com>
2020-07-27 23:35:11 +0800

    delete a brunch

--- execute.c
+++ execute.c
@@ -2,7 +2,7 @@
 
 void main(){
     
-    if(!access("info.exe",0)) system("del info.exe"); 
+    if(!access("info.exe",0)) system("del info.exe"); // delete old file
     if(!access("info.dat",0)) system("del info.dat"); 
     if(!access("info.txt",0)) system("del info.txt");  
     if(!access("title.txt",0)) system("del title.txt"); 
