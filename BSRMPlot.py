63ce41c72c2cc9e51ccdc759abea37290a88aa86
he <56215142+hezeqiang@users.noreply.github.com>
2020-10-28 13:39:56 +0900

    Update BSRMPlot.py

--- BSRMPlot.py
+++ BSRMPlot.py
@@ -1,5 +1,8 @@
 #coding:u8
-from pylab import plt, mpl, np
+#from pylab import plt, mpl, np
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 # from pprint import pprint
